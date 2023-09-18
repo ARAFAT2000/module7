88cd18c3b6657303994dcb2c92ce02eacb347574
ARAFAT2000 <eee1916043brur@gmail.com>
2023-09-18 11:45:53 +0600

    project finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'assaignment/assaignment.dart';
 import 'livequize/quize.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 debugShowCheckedModeBanner: false,
-      home:Quize6(),
+      home:Assaignment(),
     );
   }
 }
